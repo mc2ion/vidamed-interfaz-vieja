43c060885ee93bb7a166750ac5d9b3afa00d223a
Marion <mc2.ions@gmail.com>
2013-10-04 10:14:19 -0430

    cambie el menu. Formulario crear protocolo

---
+++ WebContent/js/formToWizard.js
@@ -0,0 +1,82 @@
+/* Created by jankoatwarpspeed.com */
+
+(function($) {
+    $.fn.formToWizard = function(options) {
+        options = $.extend({  
+            submitButton: "" 
+        }, options); 
+        
+        var element = this;
+
+        var steps = $(element).find("fieldset");
+        var count = steps.size();
+        var submmitButtonName = "#" + options.submitButton;
+        $(submmitButtonName).hide();
+        var typeProtocol = "";
+
+        // 2
+        $(element).before("<ul id='steps'></ul>");
+
+        steps.each(function(i) {
+            $(this).wrap("<div id='step" + i + "'></div>");
+            $(this).append("<p id='step" + i + "commands'></p>");
+
+            // 2
+            var name = $(this).find("legend").html();
+            $("#steps").append("<li id='stepDesc" + i + "'>" + name + "</li>");
+
+            if (i == 0) {
+                createNextButton(i);
+                selectStep(i);
+            }
+            else if (i == count - 1) {
+                $("#step" + i).hide();
+                createPrevButton(i);
+            }
+            else {
+                $("#step" + i).hide();
+                createPrevButton(i);
+                createNextButton(i);
+            }
+        });
+
+        function createPrevButton(i) {
+            var stepName = "step" + i;
+			$("#" + stepName + "commands").append("<input id='" + stepName + "Prev'  type='button' value='< Anterior' class='prev'>");
+          
+            $("#" + stepName + "Prev").bind("click", function(e) {
+                $("#" + stepName).hide();
+                $("#step" + (i - 1)).show();
+                $(submmitButtonName).hide();
+                selectStep(i - 1);
+            });
+        }
+
+        function createNextButton(i) {
+            var stepName = "step" + i;
+            $("#" + stepName + "commands").append("<input id='" + stepName + "Next' type='button' value='Siguiente >' class='next'>");
+
+            $("#" + stepName + "Next").bind("click", function(e) {
+            	typeProtocol = $("#typeOfProtocol").val()
+            	alert(typeProtocol);
+            	if (typeProtocol != 'A'){
+            		$("#tr2").hide();
+            		$("#tr3").hide();
+            		$("#tr4").hide();
+            		$("#tr5").hide();
+            	}
+                $("#" + stepName).hide();
+                $("#step" + (i + 1)).show();
+                if (i + 2 == count)
+                    $(submmitButtonName).show();
+                selectStep(i + 1);
+            });
+        }
+
+        function selectStep(i) {
+            $("#steps li").removeClass("current");
+            $("#stepDesc" + i).addClass("current");
+        }
+
+    }
+})(jQuery); 
