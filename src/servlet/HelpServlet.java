f00755fc0aad104f0bd8f5d9b8db39848a407971
Yomar Medina <ailema39@gmail.com>
2018-05-28 18:55:20 -0500

    Cambios configuracion/correccion errores

--- src/servlet/HelpServlet.java
+++ src/servlet/HelpServlet.java
@@ -8,10 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import command.CommandExecutor;
-import domain.User;
 
 
 
