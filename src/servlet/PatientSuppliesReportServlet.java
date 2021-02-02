ab549f53cd7619980f1d5f9d81b90f1b4da377ec
Yomar Medina <ailema39@gmail.com>
2021-02-01 22:46:49 -0500

    Reporte de insumos pacientes

---
+++ src/servlet/PatientSuppliesReportServlet.java
@@ -0,0 +1,107 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import command.CommandExecutor;
+import domain.BilledSupply;
+import domain.PermissionsList;
+import domain.User;
+
+/**
+ * Servlet implementation class PatientSuppliesReportServlet
+ */
+@WebServlet(description = "servlet to patient supplies report", urlPatterns = { "/PatientSuppliesReportServlet" })
+public class PatientSuppliesReportServlet extends HttpServlet {	
+	private static final long serialVersionUID = 1L;
+	
+	public void init() throws ServletException {
+		super.init();
+		try {
+			CommandExecutor.getInstance();
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+	}
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PatientSuppliesReportServlet() {
+        super();
+    }
+    
+    /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		User userE = (User)session.getAttribute("user");
+		boolean perm  = PermissionsList.hasPermission(request, PermissionsList.reports);
+		
+		if(userE != null && perm ){
+			try {				
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/reports_patientsupplies.jsp");
+				rd.forward(request, response);
+			} 
+			catch (Exception e) {
+				throw new ServletException(e);
+			}
+		} else {
+			if (userE == null){
+				request.setAttribute("time_out", "Su sesión ha expirado. Ingrese nuevamente"); RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
+				rd.forward(request, response);
+			}else{
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/sectionDenied.jsp");
+				rd.forward(request, response);
+			}
+		}	
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		User userE = (User)session.getAttribute("user");
+		
+		
+		boolean perm  = PermissionsList.hasPermission(request, PermissionsList.reports);
+		
+		if(userE != null && perm ){	
+			String from 	= request.getParameter("from");
+			String to	 	= request.getParameter("to");
+			try {
+				
+				@SuppressWarnings("unchecked")
+				ArrayList<BilledSupply> pp = (ArrayList<BilledSupply>) CommandExecutor.getInstance().executeDatabaseCommand(new command.GetPatientSuppliesReport(from, to));
+				request.setAttribute("pp", pp);
+				
+				request.setAttribute("from", from);
+				request.setAttribute("to", to);
+								
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/reports_patientsupplies.jsp");
+				rd.forward(request, response);
+			} 
+			catch (Exception e) {
+				throw new ServletException(e);
+			}
+		} else {
+			if (userE == null){
+				request.setAttribute("time_out", "Su sesión ha expirado. Ingrese nuevamente"); RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
+				rd.forward(request, response);
+			}else{
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/sectionDenied.jsp");
+				rd.forward(request, response);
+			}
+		}	
+	}
+}
